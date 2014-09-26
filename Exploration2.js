724268772b9fe97c0f5026026d67ddfcd5e5e24f
Tyron Allen <Zuezs@users.noreply.github.com>
2014-09-25 23:52:09 -0500

    Create Exploration2.js

---
+++ Exploration2.js
@@ -0,0 +1,50 @@
+x = 10
+"Tyron"
+
+"Tyron".length
+
+12 + 8
+
+if(8 * 12 === 200 ){}
+	console.log("This is amazing!!")
+}
+else{
+	console.log("The number is incorrect!!")
+}
+
+confirm('Lets move onto the next step')
+
+prompt('Who is the greatest?')
+
+// Check if the user is ready to play!
+
+confirm("I am ready to play")
+age = prompt("What's your age")
+
+if(age < 13){
+    console.log('You can play but your not responsible')
+}
+else{
+    console.log('Play on!')
+}
+
+console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'")
+
+console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'")
+
+userAnswer = prompt("Do you want to race Bieber on stage?")
+
+if(userAnswer === 'yes'){
+    console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!")
+}
+else{
+    console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'")
+}
+
+feedback = prompt('Rate this game out of 10')
+if(feedback > 8){
+    console.log("Thank you! We should race at the next concert!")
+}
+else{
+    console.log('I`ll keep practicing coding and racing')
+}
